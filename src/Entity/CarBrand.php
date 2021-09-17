1f309428b7f6d133a3ccaace65525d609538a5d1
Rafał Wesołowski <r.wesolowski@nexus-united.com>
2021-09-18 00:09:18 +0200

    Trust me, I'm an engineer!... What the f*ck did just happened here?

--- src/Entity/CarBrand.php
+++ src/Entity/CarBrand.php
@@ -24,6 +24,16 @@ class CarBrand
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Car::class, mappedBy="brand")
+     */
+    private $cars;
+
+    public function __construct()
+    {
+        $this->cars = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -37,6 +47,36 @@ class CarBrand
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Car[]
+     */
+    public function getCars(): Collection
+    {
+        return $this->cars;
+    }
+
+    public function addCar(Car $car): self
+    {
+        if (!$this->cars->contains($car)) {
+            $this->cars[] = $car;
+            $car->setBrand($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCar(Car $car): self
+    {
+        if ($this->cars->removeElement($car)) {
+            // set the owning side to null (unless already changed)
+            if ($car->getBrand() === $this) {
+                $car->setBrand(null);
+            }
+        }
 
         return $this;
     }
